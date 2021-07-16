f5ca5762335da9894aea10c953e895be632cafe7
nguyenduc2511 <nguyenduc251196@gmail.com>
2021-07-16 17:19:10 +0700

    bai_1

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # C0421G1_NguyenVanDuc_Module3
 
 cố gắng vượt qua module3 tốt hơn cái 2 nào!! làm lại và cẩn thận hơn nữa.
+724188duc pass.
---
+++ _01_CSDL_quan_he/practise/_03_Student_management.sql
@@ -0,0 +1,31 @@
+create database student_management;
+use student_management;
+
+create table student_management.student(
+id int auto_increment primary key,
+`name` varchar(50),
+age int null,
+country varchar(45)
+);
+ alter table student
+ add date_of_birth date;
+ 
+ insert into student 
+value (1,'nguyen van duc',25, 'da nang', '1996-11-25');
+insert into student (`name`,age,country,date_of_birth)
+value ('nguyen van hai',25, 'da nang', '2000-11-25');
+insert into student (`name`,country,date_of_birth)
+value ('nguyen van ba', 'da nang', '1998-11-25');
+insert into student 
+value (5,'nguyen van nam',25, 'da nang', '1999-11-25');
+insert into student (`name`,age,country,date_of_birth)
+value ('nguyen van sau',25, 'da nang', '2001-11-25');
+insert into student 
+value (4,'nguyen van bon',25, 'da nang', '2000-11-25');
+
+update student 
+set age = 28
+where id = 3;
+
+  -- truncate table student; 
+ 
